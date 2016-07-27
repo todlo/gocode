37eca6b67d540ea55caad720b9b1f193f242214f
todlo <todd.stiers@gmail.com>
2016-07-27 16:02:15 -0700

    adding more logic (in 'while < 21' loop) for high ace scenarios.

--- blackjack/draw.go
+++ blackjack/draw.go
@@ -12,7 +12,6 @@ func askYn(q string) bool {
 	fmt.Printf(q)
 	_, err := fmt.Scanln(&a)
 	strings.ToLower(a)
-	fmt.Println("DEBUG:", a)
 	switch {
 	case strings.HasPrefix("yes", a):
 		return true
@@ -45,16 +44,18 @@ func draw() (string, int) {
 	}
 	num := r.Intn(14-1) + 1
 	suit := [4]string{"Spades", "Diamonds", "Clubs", "Hearts"}
-	return fmt.Sprintln(c[num], "of", suit[r.Intn(4)]), num
+	return c[num] + " of " + suit[r.Intn(4)], num
 }
 
 func main() {
+	var highace bool
 	hand := make([]string, 2)
 
 	card1, score1 := draw()
 	if score1 > 10 { score1 = 10 }
 	if strings.Contains(card1, "Ace") {
 		score1 = score1 + 10
+		highace = true
 	}
 	hand[0] = card1
 
@@ -62,29 +63,42 @@ func main() {
 	if score2 > 10 { score2 = 10 }
 	if strings.Contains(card2, "Ace") && score1 < 11 {
 		score2 = score2 + 10
+		highace = true
 	}
 	hand[1] = card2
 
 	t := score1+score2
-	fmt.Println("Hand:")
+	fmt.Println("*** Hand: ***")
 	for i := range hand {
-		fmt.Print(hand[i])
+		fmt.Println(hand[i])
 	}
 	fmt.Println("Score:", t)
 
 	for t < 21 {
 		if askYn("Would you like to hit? [Y/n]: ") {
 			newcard, value := draw()
+			if strings.Contains(newcard, "Ace") && t + 11 <= 21 {
+				t = t+11
+			}
 			t = t+value
 			hand = append(hand, newcard)
 			fmt.Printf("Dealer deals a %s.\nNew score: %d\n", newcard, t)
 		} else {
 			break
 		}
+		switch {
+		case highace == true && t > 21:
+			highace = false
+			t = t-10
+		case t == 21:
+			fmt.Println("BLACKJACK!! :D")
+		case t > 21:
+			fmt.Println("BUSTED! :(")
+		}
 	}
 	fmt.Println("Final score:", t)
 	fmt.Println("Final hand:")
 	for i := range hand {
-		fmt.Print(hand[i])
+		fmt.Println(hand[i])
 	}
 }
