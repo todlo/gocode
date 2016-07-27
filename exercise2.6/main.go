26c2a81d2c8ee73af6324f0dac1626ff999a4de9
todlo <todd.stiers@gmail.com>
2016-07-27 11:58:11 -0700

    Moved synopsis to top. Removed extraneous == true evaluators.

--- exercise2.6/main.go
+++ exercise2.6/main.go
@@ -1,3 +1,8 @@
+// https://projecteuler.net/problem=36
+// "The decimal number, 585 = 1001001001 [in base 2] (binary), is palindromic in both bases.
+// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+// (Please note that the palindromic number, in either base, may not include leading zeros.)"
+// Author: Todd S.
 package main
 
 import (
@@ -24,9 +29,9 @@ func main() {
 	counter := 0
 	for i := 1; i <=1000000; i++ {
 		a := fmt.Sprintf("%v", i)
-		if atest(a) == true {
+		if atest(a) {
 			b := fmt.Sprintf("%b", i)
-			if btest(b) == true {
+			if btest(b) {
 				fmt.Printf("%v is palindromic in both base 10 and "+
 				"base 2 (%b).\n", i, i)
 				counter += i
@@ -36,9 +41,3 @@ func main() {
 	fmt.Printf("The total of all numbers between 1 and 1,000,000 "+
 	"that are palindromic in both base 2 and base 10 is %v.\n", counter)
 }
-
-// https://projecteuler.net/problem=36
-// "The decimal number, 585 = 1001001001 [in base 2] (binary), is palindromic in both bases.
-// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
-// (Please note that the palindromic number, in either base, may not include leading zeros.)"
-// Author: Todd S.
