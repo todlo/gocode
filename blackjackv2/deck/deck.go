79189c3da0cd358a1656642d7926ea2edca664d2
todlo <todd.stiers@gmail.com>
2016-09-12 18:48:36 -0700

    go fmt appeasements.

--- blackjackv2/deck/deck.go
+++ blackjackv2/deck/deck.go
@@ -10,19 +10,19 @@ import (
 func Deck() []string {
 	var deck []string
 	c := map[int]string{
-		1:"Ace",
-		2:"Two",
-		3:"Three",
-		4:"Four",
-		5:"Five",
-		6:"Six",
-		7:"Seven",
-		8:"Eight",
-		9:"Nine",
-		10:"Ten",
-		11:"Jack",
-		12:"Queen",
-		13:"King",
+		1:  "Ace",
+		2:  "Two",
+		3:  "Three",
+		4:  "Four",
+		5:  "Five",
+		6:  "Six",
+		7:  "Seven",
+		8:  "Eight",
+		9:  "Nine",
+		10: "Ten",
+		11: "Jack",
+		12: "Queen",
+		13: "King",
 	}
 	suit := [4]string{"Clubs", "Diamonds", "Hearts", "Spades"}
 
