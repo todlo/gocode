ce8d370560b5d606ea3f8c0eb3269b5739a3873f
todlo <todd.stiers@gmail.com>
2016-09-14 22:13:19 -0700

    I have no idea what I'm doing. I spent like 3 hours trying to make a struct happen by assigning the contents of x[i] to player{getNames[i]}, but I don't think that's how that works.. as much as I want it to. Settled on the below for now.

--- bowling/main.go
+++ bowling/main.go
@@ -10,6 +10,10 @@ import (
 	"fmt"
 )
 
+type player struct {
+	name string
+}
+
 func bowl(set []int) {
 	var roll1, roll2, score int
 	frame := 1
@@ -40,11 +44,41 @@ func bowl(set []int) {
 	fmt.Println("Final Score:", score)
 }
 
+func getPlayers() int {
+	var a int
+	fmt.Print("How many players? [1-8]: ")
+	_, err := fmt.Scan(&a)
+	if err != nil || a < 1 || a > 8 {
+		fmt.Println(fmt.Sprint(err)+": Please pick a number from 1 through 8.")
+		getPlayers()
+	}
+	return a
+}
+
+func getNames(p int) string {
+	var a string
+	fmt.Printf("What is the name of player %d? ", p)
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println(err, "Something went wrong.")
+		getNames(p)
+	}
+	return a
+}
+
 func main() {
+	numplayers := getPlayers()
+	x := make([]string, numplayers)
+	fmt.Printf("You entered %d players.\n", numplayers)
+	for i := 0; i < numplayers; i++ {
+		x[i] = fmt.Sprintf(getNames(i+1))
+		fmt.Printf("Player %d is %s.\n", i+1, x[i])
+	}
+	fmt.Println(x[0])
 	p1scores := []int{5, 3, 6, 4, 5, 4, 7, 1, 5, 5, 9, 0, 3, 7, 10, 8, 0, 7, 2}
-	p2scores := []int{5, 3, 10, 5, 4, 7, 1, 5, 5, 9, 0, 3, 7, 10, 8, 0, 7, 2}
-	p3scores := []int{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}
+	//p2scores := []int{5, 3, 10, 5, 4, 7, 1, 5, 5, 9, 0, 3, 7, 10, 8, 0, 7, 2}
+	//p3scores := []int{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}
 	bowl(p1scores)
-	bowl(p2scores)
-	bowl(p3scores)
+	//bowl(p2scores)
+	//bowl(p3scores)
 }
