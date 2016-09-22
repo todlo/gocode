ba0bb56bdcb26a8be7d52f6c6d7b7e99199faa15
todlo <todd.stiers@gmail.com>
2016-09-21 22:06:16 -0700

    Building an IPv4 subnet calculator... Right now all it does is parse a given v4 address with subnet in CIDR notation. Plan is to calculate and print range of available addresses in that block.

---
+++ subcalc/main.go
@@ -0,0 +1,26 @@
+// subcalc takes a given IPv4 address with subnet and returns
+// the range of usable addresses in that subnet.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	address := make([]string, 4)
+	var a string
+	fmt.Print("Enter v4 address with subnet (e.g., 1.2.3.4/24): ")
+	fmt.Scanln(&a)
+	sub := a[strings.Index(a, "/")+1:]
+	a = a[:strings.Index(a, "/")]
+	for i := 0; i < 3; i++ {
+		address[i] = a[:strings.Index(a, ".")]
+		a = a[strings.Index(a, ".")+1:]
+	}
+	address[3] = a
+	address = append(address, sub)
+
+	fmt.Println(address)
+}
