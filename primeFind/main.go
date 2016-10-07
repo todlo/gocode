9ba5ca9d667f07f52d6abe3e5ae7aaf871b0ab5d
todlo <todd.stiers@gmail.com>
2016-10-07 14:21:42 -0700

    Made a prime number finder. (*During a meeting that I really should have been paying attention to. Alas.)

---
+++ primeFind/main.go
@@ -0,0 +1,23 @@
+// primeFind is a little program that finds all
+// the prime numbers between 2 and 10,000.
+// (I made this during a meeting that I really
+// should have been paying attention to. Alas.)
+// Author: Todd S.
+package main
+
+import "fmt"
+
+func main() {
+	var count int
+	for i := 2; i <= 10000; i++ {
+		for j := 1; j <= i; j++ {
+			if i%j == 0 {
+				count++
+			}
+		}
+		if count <= 2 {
+			fmt.Println(i, "is prime.")
+		}
+		count = 0
+	}
+}
