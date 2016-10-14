7d7d6fefffb1f8bb95ceb5929437af5cf5e22c58
todlo <todd.stiers@gmail.com>
2016-10-13 21:24:15 -0700

    Final(ish) solution.  - Created rev function that takes a slice of runes and returns a slice of runes in reverse order.  - Created index so that I know where to put them back in the word.  - Other stuff I can't remember.. there was a lot of debug output & general bouncing off of the walls until I magically landed here.

--- vowelsOfHell/main.go
+++ vowelsOfHell/main.go
@@ -8,19 +8,33 @@ import (
 	"strings"
 )
 
-//func rev(a []string) []string {
-//}
+func rev(a []rune) []rune {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+	return a
+}
 
 func main() {
 	vowels := "aeiouy"
-	thearg := os.Args[1]
-	bytearg := []rune(thearg)
-	fmt.Println(string(bytearg))
-	for j := range bytearg {
+	thearg := []byte(os.Args[1])
+	var vcount int
+	var argv []rune
+	var index []int
+	for j := range []rune(os.Args[1]) {
 		for i := 0; i < 6; i++ {
-			if strings.IndexRune(string(bytearg[j]), []rune(vowels)[i]) > -1 {
+			if strings.IndexRune(string(os.Args[1][j]), []rune(vowels)[i]) > -1 {
 				fmt.Println(string(vowels[i]), "found in position", j)
+				vcount++
+				argv = append(argv, rune(vowels[i]))
+				index = append(index, j)
 			}
 		}
 	}
+	fmt.Printf("There are %d vowels in the word \"%s\".\n", vcount, os.Args[1])
+	rargv := rev(argv)
+	for i := range rargv {
+		thearg[index[i]] = byte(rargv[i])
+	}
+	fmt.Println(string(thearg))
 }
